f770c44cd79cac5765441a7d985825c2de9c0574
junho <kopqwe147@gmail.com>
2023-09-29 03:41:19 +0900

    feat: UserRepository에 중복 체크 existsByNickname 메소드 추가

--- src/main/java/com/example/pingpong/repository/UserRepository.java
+++ src/main/java/com/example/pingpong/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByNickname(String nickname);
+
+    boolean existsByNickname(String nickname);
 }
