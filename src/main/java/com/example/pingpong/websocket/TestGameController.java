6fdb580927145f9bf0f15467f6a3a3dd72919eba
jang-insu <wkddlstn132@gm>
2023-10-06 17:53:44 +0900

    with Stomp ver add

--- src/main/java/com/example/pingpong/websocket/TestGameController.java
+++ src/main/java/com/example/pingpong/websocket/TestGameController.java
@@ -3,6 +3,7 @@ package com.example.pingpong.websocket;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.util.ArrayList;
@@ -12,23 +13,31 @@ import java.util.Map;
 
 @Controller
 public class TestGameController {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public TestGameController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
     private static List<String> list = new ArrayList<>();
     private static Map<String, List<String>> gameRooms = new HashMap<>();
-
     @MessageMapping("/matchingJoin")
     public void test(SimpMessageHeaderAccessor accessor) {
         list.add(accessor.getSessionId());
         // 2명이면 게임방 만들고 2명에 이벤트 전송 후 list 초기화
         if (list.size() == 2) {
-            String gameRoomId = String.valueOf(System.currentTimeMillis());
-            gameRooms.put(gameRoomId, new ArrayList<>(list));
-            matchingSuccess(gameRoomId);
+            matchingSuccess(list);
             list.clear();
         }
         System.out.println("accessor : " + accessor.getSessionId());
     }
-    @SendTo("/topic/1")
-    public void matchingSuccess(String gameRoomId) {
-        System.out.println("옥상으로 따라와");
+    @SendTo("/topic/matching-success")
+    public void matchingSuccess(List<String> userList) {
+        String gameRoomId = String.valueOf(System.currentTimeMillis());
+        System.out.println(gameRoomId);
+        gameRooms.put(gameRoomId, userList);
+        for (String sessionId : userList) {
+            String destination = "/user/" + sessionId + "/queue/matching-success";
+            messagingTemplate.convertAndSend(destination, gameRoomId);
+        }
     }
 }
