d51336c6c94ec9708eebb6ca2ee8321360c327f3
jang-insu <wkddlstn132@gm>
2023-10-07 22:51:38 +0900

    GameInfomation timer add
    game logic add
    프론트랑 이벤트 연결만 하면 됩니다.

--- src/main/java/com/example/pingpong/game/model/GameInfomation.java
+++ src/main/java/com/example/pingpong/game/model/GameInfomation.java
@@ -1,5 +1,7 @@
 package com.example.pingpong.game.model;
 
+import java.util.concurrent.ScheduledFuture;
+
 public class GameInfomation {
     private int maxScore;
     private GameElement gameElement;
@@ -7,13 +9,15 @@ public class GameInfomation {
     private double velocityY;
     private int leftPaddleStatus;
     private int rightPaddleStatus;
+    private ScheduledFuture<?> timer;
     public GameInfomation() {
         this.gameElement = new GameElement();
-        this.maxScore = 5;
+        this.maxScore = 3;
         this.velocityX = 0.5;
-        this.velocityY = 0.5;
+        this.velocityY = 0.25;
         this.leftPaddleStatus = 0;
         this.rightPaddleStatus = 0;
+        this.timer = null;
     }
     public GameInfomation(int maxScore, GameElement gameElement, double velocityX, double velocityY, int leftPaddleStatus, int rightPaddleStatus) {
         this.maxScore = maxScore;
@@ -67,5 +71,20 @@ public class GameInfomation {
 
     public void setRightPaddleStatus(int rightPaddleStatus) {
         this.rightPaddleStatus = rightPaddleStatus;
+    }
+
+    public ScheduledFuture<?> getTimer() {
+        return timer;
+    }
+
+    public void setTimer(ScheduledFuture<?> timer) {
+        this.timer = timer;
+    }
+
+    // 타이머를 취소하는 메서드 추가
+    public void cancelTimer() {
+        if (timer != null) {
+            timer.cancel(false);
+        }
     }
 }
--- src/main/java/com/example/pingpong/websocket/TestGameController.java
+++ src/main/java/com/example/pingpong/websocket/TestGameController.java
@@ -1,6 +1,9 @@
 package com.example.pingpong.websocket;
 
+import com.example.pingpong.game.model.Ball;
+import com.example.pingpong.game.model.GameElement;
 import com.example.pingpong.game.model.GameInfomation;
+import com.example.pingpong.game.model.Paddle;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -13,6 +16,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class TestGameController {
@@ -22,6 +29,7 @@ public class TestGameController {
     }
     private static List<String> list = new ArrayList<>();
     private static Map<String, List<String>> matchUsers = new HashMap<>();
+    private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
 
     private static Map<String, GameInfomation> gameRooms = new HashMap<>();
     @MessageMapping("/matchingJoin")
@@ -52,6 +60,143 @@ public class TestGameController {
         matchUsers.put(gameRoomId, userList);
         String destination = "/topic/matching-success";
         this.gameRooms.put(gameRoomId, new GameInfomation());
+        Runnable positionUpdateTask = () -> {
+            positionUpdate(gameRoomId);
+        };
+        long initialDelay = 0;
+        long period = 1000 / 60;
+        ScheduledFuture<?> timer = executorService.scheduleAtFixedRate(positionUpdateTask, initialDelay, period, TimeUnit.MILLISECONDS);
+        this.gameRooms.get(gameRoomId).setTimer(timer);
         messagingTemplate.convertAndSend(destination, jsonObject);
+    }
+
+
+    // 여기서 부터는 게임 관련 로직 ////////
+
+
+    @MessageMapping("/positionUpdate")
+    public void positionUpdate(String roomName) {
+        System.out.println("position update");
+        GameInfomation gameRoom = this.gameRooms.get(roomName);
+        ballUpdate(gameRoom);
+        ballCollision(gameRoom);
+        boolean gameFinished = gameScoreCheck(gameRoom);
+        messagingTemplate.convertAndSendToUser(roomName, "/position_update", gameRoom.getElement());
+        if (gameFinished)
+            finishGame(gameRoom, roomName);
+    }
+
+//    @MessageMapping("/paddleMove")
+//    public void paddleMove(PaddleMoveRequest data) {
+//        GameInformation gameRoom = gameService.getGameRoom(data.getRoomName());
+//        if (gameRoom == null) {
+//            return;
+//        }
+//        if (data.isOwner()) {
+//            gameRoom.setLeftPaddleStatus(data.getPaddleStatus());
+//        } else {
+//            gameRoom.setRightPaddleStatus(data.getPaddleStatus());
+//        }
+//    }
+
+    private void ballUpdate(GameInfomation gameRoom) {
+        GameElement element = gameRoom.getElement();
+        Ball ball = element.getBall();
+
+        updatePaddlePosition(gameRoom.getLeftPaddleStatus(), element.getLeftPaddle());
+        updatePaddlePosition(gameRoom.getRightPaddleStatus(), element.getRightPaddle());
+
+        ball.setX(ball.getX() + gameRoom.getVelocityX());
+        ball.setY(ball.getY() + gameRoom.getVelocityY());
+
+        double velocityX = gameRoom.getVelocityX();
+        double velocityY = gameRoom.getVelocityY();
+        gameRoom.setVelocityX(velocityX < 0 ? velocityX - 0.01 : velocityX + 0.01);
+        gameRoom.setVelocityY(velocityY < 0 ? velocityY - 0.01 : velocityY + 0.01);
+    }
+
+    private void ballCollision(GameInfomation gameRoom) {
+        Ball ball = gameRoom.getElement().getBall();
+        Paddle leftPaddle = gameRoom.getElement().getLeftPaddle();
+        Paddle rightPaddle = gameRoom.getElement().getRightPaddle();
+
+        // 천장과 바닥 충돌 확인
+        if (ball.getY() - ball.getRadius() < 0) {
+            ball.setY(ball.getRadius());
+            gameRoom.setVelocityY(-gameRoom.getVelocityY());
+        } else if (ball.getY() + ball.getRadius() > 100) {
+            ball.setY(100 - ball.getRadius());
+            gameRoom.setVelocityY(-gameRoom.getVelocityY());
+        }
+
+        // 패들과 공의 충돌 확인
+        if (ball.getX() - ball.getRadius() < leftPaddle.getX() + leftPaddle.getWidth() &&
+                ball.getX() + ball.getRadius() > leftPaddle.getX() &&
+                ball.getY() + ball.getRadius() >= leftPaddle.getY() &&
+                ball.getY() - ball.getRadius() <= leftPaddle.getY() + leftPaddle.getHeight()) {
+            double deltaY = ball.getY() - (leftPaddle.getY() + leftPaddle.getHeight() / 2);
+            if (ball.getX() + ball.getRadius() > leftPaddle.getX() + leftPaddle.getWidth() / 2) {
+                gameRoom.setVelocityX(gameRoom.getVelocityX() < 0 ? -gameRoom.getVelocityX() : gameRoom.getVelocityX());
+            }
+            gameRoom.setVelocityY(deltaY * 0.2);
+        }
+
+        if (ball.getX() + ball.getRadius() > rightPaddle.getX() &&
+                ball.getX() - ball.getRadius() < rightPaddle.getX() + rightPaddle.getWidth() &&
+                ball.getY() + ball.getRadius() >= rightPaddle.getY() &&
+                ball.getY() - ball.getRadius() <= rightPaddle.getY() + rightPaddle.getHeight()) {
+            double deltaY = ball.getY() - (rightPaddle.getY() + rightPaddle.getHeight() / 2);
+            if (ball.getX() - ball.getRadius() < rightPaddle.getX() + rightPaddle.getWidth() / 2) {
+                gameRoom.setVelocityX(gameRoom.getVelocityX() > 0 ? -gameRoom.getVelocityX() : gameRoom.getVelocityX());
+            }
+            gameRoom.setVelocityY(deltaY * 0.2);
+        }
+    }
+
+    private void updatePaddlePosition(int status, Paddle paddle) {
+        double speed = 3.0;
+        if (status == 1 && paddle.getY() > 0) {
+            paddle.setY(paddle.getY() - speed);
+        } else if (status == 2 && paddle.getY() < 100 - paddle.getHeight()) {
+            paddle.setY(paddle.getY() + speed);
+        }
+    }
+
+    private boolean gameScoreCheck(GameInfomation gameRoom) {
+        Ball ball = gameRoom.getElement().getBall();
+        Integer leftScore = gameRoom.getElement().getLeftScore();
+        Integer rightScore = gameRoom.getElement().getRightScore();
+
+        if (ball.getX() < 0 || ball.getX() + ball.getRadius() * 2 > 100) {
+            if (ball.getX() < 0) {
+                gameRoom.getElement().setRightScore(rightScore + 1);
+            } else {
+                gameRoom.getElement().setLeftScore(leftScore + 1);
+            }
+            resetBallPosition(gameRoom);
+        }
+
+        return gameRoom.getElement().getLeftScore() == gameRoom.getMaxScore() || gameRoom.getElement().getRightScore() == gameRoom.getMaxScore();
+    }
+
+    private void resetBallPosition(GameInfomation gameRoom) {
+        Ball ball = gameRoom.getElement().getBall();
+        ball.setX(50 - ball.getRadius());
+        ball.setY(50 - ball.getRadius());
+
+        double directX = Math.random() < 0.5 ? -1.0 : 1.0;
+        double directY = Math.random() < 0.5 ? -1.0 : 1.0;
+
+        gameRoom.setVelocityX(0.5 * directX);
+        gameRoom.setVelocityY(0.25 * directY);
+    }
+
+    private void finishGame(GameInfomation gameRoom, String roomName) {
+        if (gameRoom.getTimer() != null) {
+            gameRoom.getTimer().cancel(false);
+        }
+        System.out.println("game finished");
+        System.out.println("left score: " + gameRoom.getElement().getLeftScore() + ", right score: " + gameRoom.getElement().getRightScore());
+        messagingTemplate.convertAndSendToUser(roomName, "/finish_game", roomName);
     }
 }
