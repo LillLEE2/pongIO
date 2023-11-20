897e299de141b10155f569c2e55847f88b244470
LillLEE2 <42.4.junholee@gmail.com>
2023-11-20 21:46:52 +0900

    Refactor: MatchingManager changed strategy pattern.

---
+++ src/main/java/com/example/pingpong/manager/matching/mode/NormalMatchingStrategy.java
@@ -0,0 +1,28 @@
+package com.example.pingpong.manager.matching.mode;
+
+import com.example.pingpong.game.dto.GameMode;
+import com.example.pingpong.game.dto.MatchingRequest;
+import com.example.pingpong.game.dto.MatchingResult;
+import com.example.pingpong.global.Global;
+import com.example.pingpong.manager.matching.GameMatchingStrategy;
+import com.example.pingpong.room.model.RoomType;
+import com.example.pingpong.user.dto.UserQueue;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class NormalMatchingStrategy implements GameMatchingStrategy {
+    @Override
+    public MatchingResult match(MatchingRequest request) {
+        UserQueue userQueue = request.getUserQueue();
+        GameMode mode = request.getGameMode();
+        RoomType type = request.getRoomType();
+
+        if (Global.NORMAL_MODE_QUEUE.size() == 2) {
+            ConcurrentLinkedQueue<UserQueue> queue = new ConcurrentLinkedQueue<>(Global.NORMAL_MODE_QUEUE);
+            Global.NORMAL_MODE_QUEUE.clear();
+            return new MatchingResult(true, mode, queue, type);
+        } else {
+            return new MatchingResult(false, mode, Global.NORMAL_MODE_QUEUE, type);
+        }
+    }
+}
